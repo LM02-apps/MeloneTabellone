03ae974bb14000391aa22918fc2acbfa0f9d7b3b
LM02-apps <lorenzoita29@gmail.com>
2021-10-13 09:41:17 +0200

    2nd commit

--- pages/risultati.php
+++ pages/risultati.php
@@ -1,14 +1,48 @@
 <?php
-    $array1=array($_POST["nomestudente"],$_POST["sesso"],$_POST["materia"]);
-    $array2=array($array1);
 
-   var_dump($array2);
-   echo "<br>";
+    
+    $array=array(
+        
+        $a=array($_POST["nomestudente"],$_POST["sesso"],$_POST["materia"])
+        $b=array()
+    );
+
+    if (is_null($array[0][2]))
+    {
+        
+        echo "<p>Risultato di".$array[0][0].": <b>ammesso</b>";
+    }
+    else if(count($array[0][2])<=2)
+    {
+
+        echo "<p>Risultato di".$array[0][0].": <b>ammesso</b> con debiti in ";
+        foreach ($array[0][2] as $item) {
+            echo $item.",";
+           
+        };
+
+    }
+    else
+    {
+        echo "<p>Risultato di".$array[0][0].": <b>non ammesso</b>";
+    }
+    //print_r(array_values($array2));
+
+   // var_dump($array2[0[1]]);   
+    //array
+    
+
+
+
 ?>
 
 <html>
     <body>
-        
-    <a href="../index.php">Inserisci<a>
+        <br><br>  
+        <a href="../index.php">Inserisci un nuovo utente<a><br>
+        <a href="../pages/terminascrutinio.php">Termina scrutinio<a>
+            <form>
+                <input type="hidden" name="array" value="informatica">
+            </form>
     <body>
 </html>
